16bd63b00af94d8f418a87a56f7c653ad1ee66f2
David Bensadon <bensadondavidn@gmail.com>
2024-03-03 13:54:22 +0100

    modif py s1

--- week2/day1/main.py
+++ week2/day1/main.py
@@ -1,49 +1,49 @@
-# #Exercise 3 
+#Exercise 3 
 
-# 5 < 3  false
-# 3 == 3 true
-# 3 == '3' false
-# '3' > 3 error
-# 'Hello' == 'hello' false
+5 < 3  false
+3 == 3 true
+3 == '3' false
+'3' > 3 error
+'Hello' == 'hello' false
 
 #Exercise 4
 
-# computer_brand = 'Apple'
-# print(f'I have an {computer_brand} computer')
+computer_brand = 'Apple'
+print(f'I have an {computer_brand} computer')
 
 #Exercise 5
 
-# name = 'David'
-# age = '24'
-# shoe_size = '41'
-# info = 'My name is '+ name + ', I am ' + age + ' and my shoe size is ' + shoe_size
-# print(info)
+name = 'David'
+age = '24'
+shoe_size = '41'
+info = 'My name is '+ name + ', I am ' + age + ' and my shoe size is ' + shoe_size
+print(info)
 
 #Exercise 6
 
-# a = 21
-# b = 20
-# if a > b:
-#     print('Hello World')
+a = 21
+b = 20
+if a > b:
+    print('Hello World')
 
-# #Exercise 7
+#Exercise 7
 
-# user = int(input('Pick a number'))
-# if user%2 == 0:
-#     print(f'This number is even')
-# else:
-#     print(f'This number is odd')
+user = int(input('Pick a number'))
+if user%2 == 0:
+    print(f'This number is even')
+else:
+    print(f'This number is odd')
 
 #Exercise 8
 
-# username = input('What is your name ?')
-# if username == 'David':
-#     print('We share the same name !')
-# else: print('You\'r not as lucky as me :/')
+username = input('What is your name ?')
+if username == 'David':
+    print('We share the same name !')
+else: print('You\'r not as lucky as me :/')
 
-# Exercise 8
+#Exercise 8
 
-# height = int(input('how tall are you in inches ?'))
-# if height > 145:
-#     print('You are tall enough to ride.')
-# else: print('You need to grow a little more before to ride.')
+height = int(input('how tall are you in inches ?'))
+if height > 145:
+    print('You are tall enough to ride.')
+else: print('You need to grow a little more before to ride.')
