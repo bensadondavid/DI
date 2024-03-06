732d92617dd63108c20f36bd191c832ab3b61bef
David Bensadon <bensadondavidn@gmail.com>
2024-03-06 23:07:08 +0100

    Daily challenge

--- week2/day4/dailychallenge.py
+++ week2/day4/dailychallenge.py
@@ -1,3 +1,20 @@
 
-This is  Matrix    
+# This is  Matrix    
+
+
+matrix = [
+    ['T', 'h', 'i', 's', '$' '#', '^'],
+    ['i', 's', '%', ' ', '%', 'M', 'a', 't', 'r'],
+    ['i', 'x', '?', '#', ' ', ' ', '%', '!'],
+]
+
+def my_matrix():
+    alpha_characters = []
+    for list in matrix:
+        for items in list:
+            if items.isalpha():
+             alpha_characters.append(items)
+             print(alpha_characters)
+
+my_matrix()
 
