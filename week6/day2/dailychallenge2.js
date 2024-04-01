101482c67673c79d66aeadf3cf19e422d394826a
David Bensadon <bensadondavidn@gmail.com>
2024-04-01 19:43:04 +0300

    Dailly challenge2

---
+++ week6/day2/dailychallenge2.js
@@ -0,0 +1,78 @@
+// Instructions
+
+// Part I
+
+// Create a function getCarHonda(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// The function should
+// loop through the array of object and return the first car with the name “Honda”.
+// then, return a string in the format This is a {car_make} {car_model} from {car_year}.
+// Hint : Find an array method that returns the value of the first element in an array that pass a test.
+// Use the cars inventory below:
+// const inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+// ];
+
+
+// Part II
+
+// Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// the function should return an inventory that is sorted by car_year, ascending.
+// Hint : Check out this tutorial on the sort method
+// Use the cars inventory below:
+
+// const inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+// ];
+
+
+// The output should be
+
+// [
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+// ];
+
+
+const inventory = [
+    { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+    { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+    { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+    { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+    { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  ];
+
+
+function getCarHonda(inventory){
+    
+
+    for(item of inventory){
+        if(item.car_make == 'Honda'){
+            return `This is a ${item.car_make} ${item.car_model} from ${item.car_year}.`
+        }
+    }
+        
+    
+}
+console.log(getCarHonda(inventory));
+
+
+
+function sortCarInventoryByYear(inventory){
+        inventory.sort((younger, older) => {
+            return younger.car_year - older.car_year
+            })
+return inventory
+
+}
+console.log(sortCarInventoryByYear(inventory))
