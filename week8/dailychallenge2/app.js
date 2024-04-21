b33ef349f0da27a79df0ade00780d369dfa0c126
David Bensadon <bensadondavidn@gmail.com>
2024-04-21 22:15:29 +0300

    daily challenge 2nd attempt

---
+++ week8/dailychallenge2/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const router = require('./route') 
+
+app.use(express.json())
+
+app.use('/', router)
+
+
+app.listen(4000, (req, res)=>{
+    console.log('Server is listening on 4000');
+})
---
+++ week8/dailychallenge2/route.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+
+const triviaQuestions = [
+    {
+      question: "What is the capital of France?",
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      answer: "Mars",
+    },
+    {
+      question: "What is the largest mammal in the world?",
+      answer: "Blue whale",
+    },
+  ];
+  
+let index = 0
+let score = 0
+
+router.get('/quizz',(req, res)=>{
+    
+    if(index < triviaQuestions.length){
+    let question = triviaQuestions[index].question.toLowerCase()
+       res.json({question : question})
+    }
+    else{
+        res.json({message : 'Quizz Over'});
+    }
+})
+
+router.post('/quizz', (req, res)=>{
+
+    let answer = triviaQuestions[index].answer.toLowerCase()
+    if(req.body.answer === answer){
+        score++
+        index++
+        res.json({message : 'Great answer !'})
+    }
+    else{
+        res.json({message : 'Wrong answer'})
+    }
+})
+
+router.get('/quizz/score', (req, res)=>{
+    if(index === triviaQuestions.length){
+        finalScore = score
+        res.json({'score' : finalScore })
+    }
+    else{
+        res.json({message : 'The quizz not over yet'})
+    }
+})
+
+module.exports = router
