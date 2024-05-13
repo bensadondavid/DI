b4dc1669ea1e81e973de9c1c3d32550afefc4e64
David Bensadon <bensadondavidn@gmail.com>
2024-05-13 13:32:39 +0300

    dailychallenge

---
+++ week9/day5/MiniProject/dailychallenge/src/Calculator.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+
+
+const Calculator = ()=>{
+
+    const [FirstNumber, setFirstNumber] = useState()
+    const [SecondNumber, setSecondNumber] = useState()
+    const [operation, setOperation] = useState()
+    const [resultOP, setResultOP] = useState()
+
+
+
+    const HandleOperation = (e)=>{
+        if(operation == 'add'){
+       const result = Number(FirstNumber) + Number(SecondNumber)
+       console.log(result);
+       setResultOP(result)
+        }
+        else if(operation == 'substract'){
+            const result = Number(FirstNumber) - Number(SecondNumber)
+            console.log(result);
+            setResultOP(result)
+        }
+        else if(operation == 'multiply'){
+            const result = Number(FirstNumber) * Number(SecondNumber)
+            console.log(result);
+            setResultOP(result)
+        }
+        else if(operation == 'divise'){
+            const result = Number(FirstNumber) / Number(SecondNumber)
+            console.log(result);
+            setResultOP(result)
+        }
+    }
+
+
+
+
+    return(
+        <>
+        <div className="container">
+            <select name="operations" id="operations" onChange={(e)=>{setOperation(e.target.value)}}>
+                    <option>Select an operation</option>
+                    <option value='add' >Add</option>
+                    <option value='substract'>Substract</option>
+                    <option value='multiply' >Multiply</option>
+                    <option value='divise'>Divise</option>
+            </select>
+
+            <h3>Operation beetween 2 numbers</h3>
+
+            <div className="fields">
+                <input onChange={(e)=>{setFirstNumber(e.target.value)}} type="number" />
+                <input onChange={(e)=>{setSecondNumber(e.target.value)}} type="number" /><br />
+            </div>
+            <button onClick={HandleOperation}>Operation</button>
+            <h1> {resultOP} </h1>
+
+        </div>
+
+        </>
+    )
+}
+
+export default Calculator
